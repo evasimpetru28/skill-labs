71f4a3a66d6342bd3f1f36c06ff1aa01d27854d3
evasimpetru28 <eva.simpetru@gmail.com>
2023-05-27 00:41:23 +0300

    Refactoring + fixed frontend for All Skills page

--- student/src/main/resources/static/js/all-skills.js
+++ student/src/main/resources/static/js/all-skills.js
@@ -1,23 +1,30 @@
-// var addModal = document.getElementById('addSkill');
-// addModal.addEventListener('hidden.bs.modal', function (e) {
-//     $(this).find('form').trigger('reset');
-//     $(this).find('form').find('#description').removeClass('active');
-// });
+var addModal = document.getElementById('addSkill');
+addModal.addEventListener('hidden.bs.modal', function (e) {
+    $(this).find('form').trigger('reset');
+    $(this).find('form').find('#description').removeClass('active');
+});
 
 function getStars(callingElementId) {
     let idLength = callingElementId.length;
     let starsNumber = parseInt(callingElementId.substring(idLength - 1, idLength));
+    let nameLength;
+
     if (callingElementId.substring(idLength - 2, idLength) === '10') {
         starsNumber = parseInt(callingElementId.substring(idLength - 2, idLength));
+        nameLength = idLength - 3;
+    } else {
+        nameLength = idLength - 2;
     }
     if (isNaN(starsNumber)) {
         starsNumber = 0;
+        nameLength = idLength;
     }
 
     let starsDiv = document.getElementById(callingElementId);
     for (let i = 1; i <= starsNumber; i++) {
         let iTag = document.createElement("i");
         iTag.className = "fa-solid fa-star fa-fw";
+        iTag.style = "color: #f5d836;";
         starsDiv.appendChild(iTag);
         starsDiv.append('\n');
     }
@@ -28,7 +35,15 @@ function getStars(callingElementId) {
         starsDiv.appendChild(iTag);
         starsDiv.append('\n');
     }
-    console.log(starsNumber + ' ' + callingElementId);
+
+    let trashCanDiv = document.getElementById(callingElementId.substring(0, nameLength) + "-trash-can");
+    console.log(nameLength);
+    console.log(callingElementId.substring(0, nameLength) + "-trash-can");
+
+    let trashIcon = document.createElement("i");
+    trashIcon.className = "fa-regular fa-trash-can";
+    trashIcon.style = "color: #b42727;";
+    trashCanDiv.appendChild(trashIcon);
 }
 
 function loadStars() {
