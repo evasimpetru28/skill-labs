31828432a2ad6ddc0427bf33b62154b8e2f7f15d
evasimpetru28 <eva.simpetru@gmail.com>
2023-05-31 22:58:23 +0300

    Description popup works as expected

--- student/src/main/java/com/example/student/model/SkillEvaluationInterface.java
+++ student/src/main/java/com/example/student/model/SkillEvaluationInterface.java
@@ -1,8 +1,4 @@
 package com.example.student.model;
 
 public interface SkillEvaluationInterface {
-	String getId();
-	String getName();
-	String getDescription();
-	String getCategory();
 }
---
+++ student/src/main/java/com/example/student/model/SkillInterface.java
@@ -0,0 +1,8 @@
+package com.example.student.model;
+
+public interface SkillEvaluationInterface {
+	String getId();
+	String getName();
+	String getDescription();
+	String getCategory();
+}
